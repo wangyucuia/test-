c2fa3c280541161a0aed2b28ce17f81cc9f8ebd9
ayilimi <m15201386258@163.com>
2018-07-28 16:06:40 +0800

    加注释

---
+++ test12/test12/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test12
+//
+//  Created by WYC on 2018/7/28.
+//  Copyright © 2018年 WYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test12/test12/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test12
+//
+//  Created by WYC on 2018/7/28.
+//  Copyright © 2018年 WYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
